d42cef64c545c309ca6aea10a62a504f178e8268
franciscoabsampaio <franciscoabsampaio@proton.me>
2025-03-25 09:49:57 +0000

    Debug: print invocation result

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -15,6 +15,7 @@ def test_cli_with_default_log_level(runner, monkeypatch):
 
 def test_cli_with_default_path_to_library(runner, monkeypatch):
     # Assert that the path_to_library is the default
+    monkeypatch.delenv("VINE_LOG_LEVEL")
     monkeypatch.setenv("VINE_LOG_LEVEL", "DEBUG")
 
     result = runner.invoke(cli.cli, ['version'])
@@ -26,6 +27,7 @@ def test_cli_with_log_level_and_path(runner, tmp_path, monkeypatch):
     monkeypatch.delenv("VINE_LOG_LEVEL", "DEBUG")
 
     result = runner.invoke(cli.cli, ['-l', 'DEBUG', '-p', tmp_path, 'version'])
+    print(result.output)
     assert result.exit_code == 0
     assert '--log-level: DEBUG' in result.output
     assert f'--path-to-library: {tmp_path}' in result.output
